45d06702c76d8b633b8ded092547d6eff910c05a
EAPC <unconfigured@null.spigotmc.org>
2020-08-23 20:20:34 -0600

    v.0.0.1 quick fix

---
+++ cmake-build-debug/CMakeFiles/Super_Mario_Bros_3_c++.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Erick/Desktop/Super_Mario_Bros_3_c++/src/Main/Cards.cpp" "C:/Users/Erick/Desktop/Super_Mario_Bros_3_c++/cmake-build-debug/CMakeFiles/Super_Mario_Bros_3_c++.dir/src/Main/Cards.cpp.obj"
+  "C:/Users/Erick/Desktop/Super_Mario_Bros_3_c++/src/Main/GameTick.cpp" "C:/Users/Erick/Desktop/Super_Mario_Bros_3_c++/cmake-build-debug/CMakeFiles/Super_Mario_Bros_3_c++.dir/src/Main/GameTick.cpp.obj"
+  "C:/Users/Erick/Desktop/Super_Mario_Bros_3_c++/src/Main/Global.cpp" "C:/Users/Erick/Desktop/Super_Mario_Bros_3_c++/cmake-build-debug/CMakeFiles/Super_Mario_Bros_3_c++.dir/src/Main/Global.cpp.obj"
+  "C:/Users/Erick/Desktop/Super_Mario_Bros_3_c++/src/Main/Main.cpp" "C:/Users/Erick/Desktop/Super_Mario_Bros_3_c++/cmake-build-debug/CMakeFiles/Super_Mario_Bros_3_c++.dir/src/Main/Main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/Main/Main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "GameTick.cpp"
+
+int main(){
+    GameTick game;
+    std::cout<<"Hello World";
+    return 0;
+}
